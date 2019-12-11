35bbf0300712eee46be808c04f4c65314d2cb4c2
ryflamm <rflamm@gmail.com>
2019-12-11 09:11:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sokb4p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sokb4p)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange'},
+  { id: 12, name: 'Cyclops'},
+  { id: 13, name: 'Mr Incredible'},
+  { id: 14, name: 'Professor X'},
+  { id: 15, name: 'Wolverine'},
+  { id: 16, name: 'Batman'},
+  { id: 17, name: 'Green Lantern'},
+  { id: 18, name: 'Gambit'},
+  { id: 19, name: 'Spiderman'},
+  { id: 20, name: 'The Flash'}
+];
